76e0afc8ab316c9d35c3a12abe0fef06e03a898b
Laustsen <Oliver.Laustsen@outlook.dk>
2018-11-29 20:40:47 +0100

    imported chartjs and updated profilepage

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { TrainingPage } from './../training/training';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Chart } from 'chart.js'
 
 @Component({
   selector: 'page-profile',
@@ -7,8 +9,69 @@ import { NavController } from 'ionic-angular';
 })
 export class ProfilePage {
 
+  @ViewChild('trainingCanvas') trainingCanvas;
+
+  trainingChart: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad(){
+   
+    this.trainingChart = new Chart(this.trainingCanvas.nativeElement, {
+ 
+      type: 'line',
+      data: {
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [
+              {
+                  label: "Climbing hours",
+                  fill: true,
+                  lineTension: 0.3,
+                  backgroundColor: "rgba(75,192,192,0.2)",
+                  borderColor: "rgba(75,192,192,1)",
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: "rgba(75,192,192,1)",
+                  pointBackgroundColor: "#fff",
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                  pointHoverBorderColor: "rgba(220,220,220,1)",
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  data: [65, 59, 80, 81, 56, 55, 40],
+                  spanGaps: false,
+              },
+              {
+                label: "Intensity",
+                fill: true,
+                lineTension: 0.3,
+                backgroundColor: "rgba(192,75,75,0.2)",
+                borderColor: "rgba(192,75,75,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(192,75,75,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(192,75,75,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [30, 20, 42, 44, 52, 49, 80],
+                spanGaps: false,
+              }
+          ]
+      }
+    });
   }
 
 }
--- www/build/main.js
+++ www/build/main.js
@@ -69,7 +69,7 @@ var TabsPage = /** @class */ (function () {
         this.profileTab = __WEBPACK_IMPORTED_MODULE_5__profile_profile__["a" /* ProfilePage */];
     }
     TabsPage = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\tabs\tabs.html"*/'<ion-tabs>\n  <ion-tab [root]="newsTab" tabIcon="invoice"></ion-tab>\n  <ion-tab [root]="trainingTab" tabIcon="dumbbell"></ion-tab>\n  <ion-tab [root]="createRouteTab" style="font-size: 32px;"tabIcon="md-add"></ion-tab>\n  <ion-tab [root]="viewRouteTab" tabIcon="route"></ion-tab>\n  <ion-tab [root]="profileTab" tabIcon="user"></ion-tab>\n</ion-tabs>\n'/*ion-inline-end:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\tabs\tabs.html"*/
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\tabs\tabs.html"*/'<ion-tabs >\n  <ion-tab [root]="newsTab" tabIcon="invoice"></ion-tab>\n  <ion-tab [root]="trainingTab" tabIcon="dumbbell"></ion-tab>\n  <ion-tab [root]="createRouteTab" style="font-size: 32px;"tabIcon="md-add"></ion-tab>\n  <ion-tab [root]="viewRouteTab" tabIcon="route"></ion-tab>\n  <ion-tab [root]="profileTab" tabIcon="user"></ion-tab>\n</ion-tabs>\n'/*ion-inline-end:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\tabs\tabs.html"*/
         }),
         __metadata("design:paramtypes", [])
     ], TabsPage);
@@ -227,6 +227,8 @@ var ViewRoutePage = /** @class */ (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ProfilePage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(25);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_chart_js__ = __webpack_require__(415);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_chart_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_chart_js__);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -238,17 +240,77 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 
 
+
 var ProfilePage = /** @class */ (function () {
     function ProfilePage(navCtrl) {
         this.navCtrl = navCtrl;
     }
+    ProfilePage.prototype.ionViewDidLoad = function () {
+        this.trainingChart = new __WEBPACK_IMPORTED_MODULE_2_chart_js__["Chart"](this.trainingCanvas.nativeElement, {
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "Climbing hours",
+                        fill: true,
+                        lineTension: 0.3,
+                        backgroundColor: "rgba(75,192,192,0.2)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    },
+                    {
+                        label: "Intensity",
+                        fill: true,
+                        lineTension: 0.3,
+                        backgroundColor: "rgba(192,75,75,0.2)",
+                        borderColor: "rgba(192,75,75,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(192,75,75,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(192,75,75,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [30, 20, 42, 44, 52, 49, 80],
+                        spanGaps: false,
+                    }
+                ]
+            }
+        });
+    };
+    __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_8" /* ViewChild */])('trainingCanvas'),
+        __metadata("design:type", Object)
+    ], ProfilePage.prototype, "trainingCanvas", void 0);
     ProfilePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-profile',template:/*ion-inline-start:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\profile\profile.html"*/'<ion-header>\n  <ion-navbar color="white">\n    <ion-title>Oliver Laustsen</ion-title>\n  </ion-navbar>\n\n</ion-header>\n<ion-content>\n\n  <ion-row padding>\n    <!-- Avatar -->\n    <ion-col col-5>\n      <img src="https://scontent.fcph1-1.fna.fbcdn.net/v/t1.0-9/11222053_1030132887011170_5420656514196313693_n.jpg?_nc_cat=110&_nc_ht=scontent.fcph1-1.fna&oh=0912894cd78cd81e51d78a8c0385a6d5&oe=5CAA6392" class="avatar" style="border-radius: 100px" alt="">\n    </ion-col>\n    <!-- Information about user -->\n\n  </ion-row>\n\n  <!-- Profile info -->\n  <div padding >\n      <ion-icon style="font-size: 250px;" name="trending-up"></ion-icon>\n  </div>\n</ion-content>'/*ion-inline-end:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\profile\profile.html"*/
+            selector: 'page-profile',template:/*ion-inline-start:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\profile\profile.html"*/'<ion-header>\n  <ion-navbar color="white">\n    <ion-title>Oliver Laustsen</ion-title>\n  </ion-navbar>\n\n</ion-header>\n<ion-content>\n\n  <ion-row padding>\n    <!-- Avatar -->\n    <ion-col col-5>\n      <img src="https://scontent.fcph1-1.fna.fbcdn.net/v/t1.0-9/11222053_1030132887011170_5420656514196313693_n.jpg?_nc_cat=110&_nc_ht=scontent.fcph1-1.fna&oh=0912894cd78cd81e51d78a8c0385a6d5&oe=5CAA6392" class="avatar" style="border-radius: 100px" alt="">\n    </ion-col>\n    <ion-col col-2 offset-1>\n      <br><br><br>\n      <p>Grade</p>\n    </ion-col>\n  </ion-row>\n\n  <!-- Profile info -->\n  <div padding >\n      <ion-card>\n          <ion-card-content>\n            <canvas #trainingCanvas height="400"></canvas>\n          </ion-card-content>\n        </ion-card>\n      <!--<ion-icon style="font-size: 250px;" name="trending-up"></ion-icon>-->\n  </div>\n</ion-content>'/*ion-inline-end:"C:\Users\Olive\Documents\repos\UraniumMongoose\src\pages\profile\profile.html"*/
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]) === "function" && _a || Object])
     ], ProfilePage);
     return ProfilePage;
+    var _a;
 }());
 
 //# sourceMappingURL=profile.js.map
@@ -391,6 +453,275 @@ var MyApp = /** @class */ (function () {
 }());
 
 //# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 439:
+/***/ (function(module, exports, __webpack_require__) {
+
+var map = {
+	"./af": 292,
+	"./af.js": 292,
+	"./ar": 293,
+	"./ar-dz": 294,
+	"./ar-dz.js": 294,
+	"./ar-kw": 295,
+	"./ar-kw.js": 295,
+	"./ar-ly": 296,
+	"./ar-ly.js": 296,
+	"./ar-ma": 297,
+	"./ar-ma.js": 297,
+	"./ar-sa": 298,
+	"./ar-sa.js": 298,
+	"./ar-tn": 299,
+	"./ar-tn.js": 299,
+	"./ar.js": 293,
+	"./az": 300,
+	"./az.js": 300,
+	"./be": 301,
+	"./be.js": 301,
+	"./bg": 302,
+	"./bg.js": 302,
+	"./bm": 303,
+	"./bm.js": 303,
+	"./bn": 304,
+	"./bn.js": 304,
+	"./bo": 305,
+	"./bo.js": 305,
+	"./br": 306,
+	"./br.js": 306,
+	"./bs": 307,
+	"./bs.js": 307,
+	"./ca": 308,
+	"./ca.js": 308,
+	"./cs": 309,
+	"./cs.js": 309,
+	"./cv": 310,
+	"./cv.js": 310,
+	"./cy": 311,
+	"./cy.js": 311,
+	"./da": 312,
+	"./da.js": 312,
+	"./de": 313,
+	"./de-at": 314,
+	"./de-at.js": 314,
+	"./de-ch": 315,
+	"./de-ch.js": 315,
+	"./de.js": 313,
+	"./dv": 316,
+	"./dv.js": 316,
+	"./el": 317,
+	"./el.js": 317,
+	"./en-au": 318,
+	"./en-au.js": 318,
+	"./en-ca": 319,
+	"./en-ca.js": 319,
+	"./en-gb": 320,
+	"./en-gb.js": 320,
+	"./en-ie": 321,
+	"./en-ie.js": 321,
+	"./en-il": 322,
+	"./en-il.js": 322,
+	"./en-nz": 323,
+	"./en-nz.js": 323,
+	"./eo": 324,
+	"./eo.js": 324,
+	"./es": 325,
+	"./es-do": 326,
+	"./es-do.js": 326,
+	"./es-us": 327,
+	"./es-us.js": 327,
+	"./es.js": 325,
+	"./et": 328,
+	"./et.js": 328,
+	"./eu": 329,
+	"./eu.js": 329,
+	"./fa": 330,
+	"./fa.js": 330,
+	"./fi": 331,
+	"./fi.js": 331,
+	"./fo": 332,
+	"./fo.js": 332,
+	"./fr": 333,
+	"./fr-ca": 334,
+	"./fr-ca.js": 334,
+	"./fr-ch": 335,
+	"./fr-ch.js": 335,
+	"./fr.js": 333,
+	"./fy": 336,
+	"./fy.js": 336,
+	"./gd": 337,
+	"./gd.js": 337,
+	"./gl": 338,
+	"./gl.js": 338,
+	"./gom-latn": 339,
+	"./gom-latn.js": 339,
+	"./gu": 340,
+	"./gu.js": 340,
+	"./he": 341,
+	"./he.js": 341,
+	"./hi": 342,
+	"./hi.js": 342,
+	"./hr": 343,
+	"./hr.js": 343,
+	"./hu": 344,
+	"./hu.js": 344,
+	"./hy-am": 345,
+	"./hy-am.js": 345,
+	"./id": 346,
+	"./id.js": 346,
+	"./is": 347,
+	"./is.js": 347,
+	"./it": 348,
+	"./it.js": 348,
+	"./ja": 349,
+	"./ja.js": 349,
+	"./jv": 350,
+	"./jv.js": 350,
+	"./ka": 351,
+	"./ka.js": 351,
+	"./kk": 352,
+	"./kk.js": 352,
+	"./km": 353,
+	"./km.js": 353,
+	"./kn": 354,
+	"./kn.js": 354,
+	"./ko": 355,
+	"./ko.js": 355,
+	"./ky": 356,
+	"./ky.js": 356,
+	"./lb": 357,
+	"./lb.js": 357,
+	"./lo": 358,
+	"./lo.js": 358,
+	"./lt": 359,
+	"./lt.js": 359,
+	"./lv": 360,
+	"./lv.js": 360,
+	"./me": 361,
+	"./me.js": 361,
+	"./mi": 362,
+	"./mi.js": 362,
+	"./mk": 363,
+	"./mk.js": 363,
+	"./ml": 364,
+	"./ml.js": 364,
+	"./mn": 365,
+	"./mn.js": 365,
+	"./mr": 366,
+	"./mr.js": 366,
+	"./ms": 367,
+	"./ms-my": 368,
+	"./ms-my.js": 368,
+	"./ms.js": 367,
+	"./mt": 369,
+	"./mt.js": 369,
+	"./my": 370,
+	"./my.js": 370,
+	"./nb": 371,
+	"./nb.js": 371,
+	"./ne": 372,
+	"./ne.js": 372,
+	"./nl": 373,
+	"./nl-be": 374,
+	"./nl-be.js": 374,
+	"./nl.js": 373,
+	"./nn": 375,
+	"./nn.js": 375,
+	"./pa-in": 376,
+	"./pa-in.js": 376,
+	"./pl": 377,
+	"./pl.js": 377,
+	"./pt": 378,
+	"./pt-br": 379,
+	"./pt-br.js": 379,
+	"./pt.js": 378,
+	"./ro": 380,
+	"./ro.js": 380,
+	"./ru": 381,
+	"./ru.js": 381,
+	"./sd": 382,
+	"./sd.js": 382,
+	"./se": 383,
+	"./se.js": 383,
+	"./si": 384,
+	"./si.js": 384,
+	"./sk": 385,
+	"./sk.js": 385,
+	"./sl": 386,
+	"./sl.js": 386,
+	"./sq": 387,
+	"./sq.js": 387,
+	"./sr": 388,
+	"./sr-cyrl": 389,
+	"./sr-cyrl.js": 389,
+	"./sr.js": 388,
+	"./ss": 390,
+	"./ss.js": 390,
+	"./sv": 391,
+	"./sv.js": 391,
+	"./sw": 392,
+	"./sw.js": 392,
+	"./ta": 393,
+	"./ta.js": 393,
+	"./te": 394,
+	"./te.js": 394,
+	"./tet": 395,
+	"./tet.js": 395,
+	"./tg": 396,
+	"./tg.js": 396,
+	"./th": 397,
+	"./th.js": 397,
+	"./tl-ph": 398,
+	"./tl-ph.js": 398,
+	"./tlh": 399,
+	"./tlh.js": 399,
+	"./tr": 400,
+	"./tr.js": 400,
+	"./tzl": 401,
+	"./tzl.js": 401,
+	"./tzm": 402,
+	"./tzm-latn": 403,
+	"./tzm-latn.js": 403,
+	"./tzm.js": 402,
+	"./ug-cn": 404,
+	"./ug-cn.js": 404,
+	"./uk": 405,
+	"./uk.js": 405,
+	"./ur": 406,
+	"./ur.js": 406,
+	"./uz": 407,
+	"./uz-latn": 408,
+	"./uz-latn.js": 408,
+	"./uz.js": 407,
+	"./vi": 409,
+	"./vi.js": 409,
+	"./x-pseudo": 410,
+	"./x-pseudo.js": 410,
+	"./yo": 411,
+	"./yo.js": 411,
+	"./zh-cn": 412,
+	"./zh-cn.js": 412,
+	"./zh-hk": 413,
+	"./zh-hk.js": 413,
+	"./zh-tw": 414,
+	"./zh-tw.js": 414
+};
+function webpackContext(req) {
+	return __webpack_require__(webpackContextResolve(req));
+};
+function webpackContextResolve(req) {
+	var id = map[req];
+	if(!(id + 1)) // check for number or string
+		throw new Error("Cannot find module '" + req + "'.");
+	return id;
+};
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = 439;
 
 /***/ })
 
